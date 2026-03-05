c13937b8723bfd6030f4cb9c529ead2a23c08c1f
liling-kongbai <kong4826@qq.com>
2026-03-05 12:55:27 +0800

    feat: 新增工具节点同时删除对话节点

--- src/graph/node/common_node.py
+++ src/graph/node/common_node.py
@@ -1,23 +1,8 @@
-from langchain_core.language_models.chat_models import BaseChatModel
-from langchain_core.prompts.chat import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables.config import RunnableConfig
+from langgraph.prebuilt.tool_node import ToolNode
 
 
-async def chat_node(state, config: RunnableConfig, llm: BaseChatModel) -> dict:
-    """对话节点"""
+async def tools_node(state, config: RunnableConfig):
+    """工具节点"""
 
-    chat_prompt_template = ChatPromptTemplate.from_messages(
-        [
-            ('system', '{system_prompt}'),
-            MessagesPlaceholder(variable_name='messages'),
-        ]
-    )
-    chain = chat_prompt_template | llm
-    response = await chain.ainvoke(
-        {
-            'system_prompt': state.system_prompt,
-            'messages': state.messages,
-        },
-        config,
-    )
-    return {'messages': [response]}
+    return await ToolNode(config['configurable'].get('tools')).ainvoke(state, config)
