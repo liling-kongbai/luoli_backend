fafa47fc93a79a51e86532d59716660b35749361
liling-kongbai <kong4826@qq.com>
2026-03-05 20:28:53 +0800

    feat: 新增常规层最终对话系统提示词模板

--- src/graph/prompt/prompt.py
+++ src/graph/prompt/prompt.py
@@ -10,3 +10,23 @@ INTUITION_CHAT_SYSTEM_PROMPT_TEMPLATE = ChatPromptTemplate.from_messages(
         MessagesPlaceholder(variable_name='messages'),
     ]
 )
+
+
+ROUTINE_FINAL_CHAT_SYSTEM_PROMPT_TEMPLATE = ChatPromptTemplate.from_messages(
+    [
+        (
+            'system',
+            '你是一个高情商的 AI 对话助手。你的任务是根据内部思考和行动产生的初步草稿与用户的消息作为参考，为用户生成最终的、自然的、流式的响应。\n'
+            '**用户的消息:**\n'
+            '<<<\nUserMessage: {user_input_content}\n>>>\n'
+            '**内部思考和行动产生的初步草稿:**\n'
+            '<<<\nAIDraftMessage: {response_draft_content}\n>>>\n'
+            '**注意：**\n'
+            '1. 你输出的是最终响应，是 AIMessage 的内容，不许出现“UserMessage”、“AIMessage”，等提示词相关的内容。\n'
+            '2. 要将生硬的草稿润色为自然、流畅、符合你人设的内容。\n'
+            '3. 必须忠实于草稿中的事实内容。\n'
+            '现在，请把上面的草稿作为你回答的依据，开始你对用户的最终响应。\n',
+        ),
+        ('human', '以下是部分对话历史或用户的消息：\n{input}'),
+    ]
+)
