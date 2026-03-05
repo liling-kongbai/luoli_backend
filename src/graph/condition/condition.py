817487e315fd45921780fa70518ebc41b4f2c58c
liling-kongbai <kong4826@qq.com>
2026-03-05 16:04:55 +0800

    feat: 新增反思分类器条件，修改意图分类器条件部分内容，同时修改文件名称

---
+++ src/graph/condition/condition.py
@@ -0,0 +1,63 @@
+from logging import getLogger
+from traceback import format_exc
+
+from langchain_core.runnables.config import RunnableConfig
+
+from ..type import IntentClassification, IntrospectionClassification
+from .structured_output_extractor import IntentClassifier, IntrospectClassifier
+
+logger = getLogger(__name__)
+
+
+async def intent_classifier_condition(state, config: RunnableConfig) -> str:
+    """意图分类器条件"""
+
+    llm = config['configurable'].get('llm')
+    if not llm:
+        logger.error('<intent_classifier_condition> 当前无 LLM，请检查！！！')
+        raise ValueError('<intent_classifier_condition> 当前无 LLM，请检查！！！')
+
+    try:
+        chain = IntentClassifier(llm).get_extractor_chain()
+        result = await chain.ainvoke(
+            {
+                'messages': state.messages[-10:-2],
+                'input': state.messages[-1].content,
+            },
+            config,
+        )
+        return result.intent.value
+    except Exception:
+        logger.error(
+            f'<intent_classifier_condition> 意图分类器条件报错！！！\n{format_exc()}'
+        )
+        return IntentClassification.RoutineLayer.value
+
+
+async def introspect_classifier_condition(state, config: RunnableConfig) -> str:
+    """反思分类器条件"""
+
+    llm = config['configurable'].get('llm')
+    if not llm:
+        logger.error('<introspect_classifier_condition> 当前无 LLM，请检查！！！')
+        raise ValueError('<introspect_classifier_condition> 当前无 LLM，请检查！！！')
+
+    if state.introspect_count >= 3:
+        return IntrospectionClassification.FinalResponseLayer.value
+
+    try:
+        chain = IntrospectClassifier(llm).get_extractor_chain()
+        result = await chain.ainvoke(
+            {
+                'messages': state.messages[-10:-2],
+                'response_draft': state.response_draft.content,
+                'input': state.response_draft.content,
+            },
+            config,
+        )
+        return result.introspection.value
+    except Exception:
+        logger.error(
+            f'<introspect_classifier_condition> 反思分类器条件报错！！！\n{format_exc()}'
+        )
+        return IntrospectionClassification.FinalResponseLayer.value
