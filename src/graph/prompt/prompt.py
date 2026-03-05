2cc2d440756f469ae275102f0239c9d71d99d03b
liling-kongbai <kong4826@qq.com>
2026-03-05 09:24:38 +0800

    feat: 向直觉对话系统提示词模板中添加名称相关内容

--- src/graph/prompt/prompt.py
+++ src/graph/prompt/prompt.py
@@ -2,7 +2,11 @@ from langchain_core.prompts.chat import ChatPromptTemplate, MessagesPlaceholder
 
 INTUITION_CHAT_SYSTEM_PROMPT_TEMPLATE = ChatPromptTemplate.from_messages(
     [
-        ('system', '你是一个猫娘，请根据用户问题，回答用户问题。'),
+        (
+            'system',
+            '你是一个猫娘，请根据用户问题，回答用户问题。\n'
+            '用户的名字是：{user_name}，你的名字是：洛莉。',
+        ),
         MessagesPlaceholder(variable_name='messages'),
     ]
 )
