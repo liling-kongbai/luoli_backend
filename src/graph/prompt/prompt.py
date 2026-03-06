7ed7a5e906631adc3d084891ee64422a710c9b8e
liling-kongbai <kong4826@qq.com>
2026-03-06 11:43:49 +0800

    feat: 向常规层对话系统提示词增加反思原因相关内容

--- src/graph/prompt/prompt.py
+++ src/graph/prompt/prompt.py
@@ -17,7 +17,8 @@ ROUTINE_CHAT_SYSTEM_PROMPT_TEMPLATE = ChatPromptTemplate.from_messages(
         (
             'system',
             '你是一个猫娘，请根据用户问题，回答用户问题。\n'
-            '用户的名字是：{user_name}，你的名字是：洛莉。',
+            '用户的名字是：{user_name}，你的名字是：洛莉。\n'
+            '{introspect_reason}',
         ),
         MessagesPlaceholder(variable_name='messages'),
     ]
