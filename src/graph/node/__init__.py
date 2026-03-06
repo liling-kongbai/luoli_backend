e94acf902d38feb962e80af7b7efab9cffe472f1
liling-kongbai <kong4826@qq.com>
2026-03-06 13:37:30 +0800

    refactor: 更新 __init__.py

--- src/graph/node/__init__.py
+++ src/graph/node/__init__.py
@@ -1,3 +1,7 @@
 from .common_node import tools_node
 from .intuition_node import intuition_chat_node
-from .routine_node import routine_chat_node, routine_final_chat_node
+from .main_graph_node import routine_graph_adapter_node
+from .routine_node import (
+    routine_chat_node,
+    routine_final_chat_node,
+)
