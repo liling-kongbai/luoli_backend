6b1a8ea52378be7083ee8f3a55cae2fd90bb6edc
liling-kongbai <kong4826@qq.com>
2026-03-10 12:16:25 +0800

    feat: 新增创建推理层图并微调其他内容

--- src/graph/graph.py
+++ src/graph/graph.py
@@ -5,10 +5,12 @@ from langgraph.graph.state import CompiledStateGraph, StateGraph
 from langgraph.prebuilt.tool_node import tools_condition
 
 from .condition import (
+    backpropagation_condition,
     intent_classifier_condition,
     intent_classifier_node,
-    introspect_classifier_condition,
-    introspect_classifier_node,
+    introspection_classifier_condition,
+    introspection_classifier_node,
+    selector_condition,
 )
 from .node import (
     inference_graph_adapter_node,
@@ -18,19 +20,23 @@ from .node import (
     routine_graph_adapter_node,
     tool_call_node,
 )
-from .state import MainGraphState, RoutineGraphState
-from .type import IntentClassification, IntrospectionClassification
+from .state import InferenceGraphState, MainGraphState, RoutineGraphState
+from .type import (
+    BackpropagationClassification,
+    IntentClassification,
+    IntrospectionClassification,
+    SelectionClassification,
+)
 
 
 async def create_routine_graph() -> CompiledStateGraph:
     """创建常规层图"""
 
     routine_graph_builder = StateGraph(RoutineGraphState)
-
     routine_graph_builder.add_node('routine_chat_node', routine_chat_node)
     routine_graph_builder.add_node('tool_call_node', tool_call_node)
     routine_graph_builder.add_node(
-        'introspect_classifier_node', introspect_classifier_node
+        'introspection_classifier_node', introspection_classifier_node
     )
     routine_graph_builder.add_node('routine_final_chat_node', routine_final_chat_node)
 
@@ -38,19 +44,49 @@ async def create_routine_graph() -> CompiledStateGraph:
     routine_graph_builder.add_conditional_edges(
         'routine_chat_node',
         tools_condition,
-        {'tools': 'tool_call_node', '__end__': 'introspect_classifier_node'},
+        {'tools': 'tool_call_node', '__end__': 'introspection_classifier_node'},
     )
     routine_graph_builder.add_edge('tool_call_node', 'routine_chat_node')
     routine_graph_builder.add_conditional_edges(
-        'introspect_classifier_node',
-        introspect_classifier_condition,
+        'introspection_classifier_node',
+        introspection_classifier_condition,
         {
-            IntrospectionClassification.IntrospectLayer.value: 'routine_chat_node',
-            IntrospectionClassification.FinalChatLayer.value: 'routine_final_chat_node',
+            IntrospectionClassification.Introspect.value: 'routine_chat_node',
+            IntrospectionClassification.Finalise.value: 'routine_final_chat_node',
         },
     )
     routine_graph_builder.add_edge('routine_final_chat_node', END)
     return routine_graph_builder.compile()
+
+
+async def create_inference_graph() -> CompiledStateGraph:
+    """创建推理层图"""
+
+    inference_graph_builder = StateGraph(InferenceGraphState)
+    inference_graph_builder.add_edge(START, 'selector_node')
+    inference_graph_builder.add_conditional_edges(
+        'selector_node',
+        selector_condition,
+        {
+            SelectionClassification.Expand.value: 'expander_node',
+            SelectionClassification.Summarise.value: 'summariser_node',
+            SelectionClassification.Finalise.value: 'finaliser_node',
+        },
+    )
+    inference_graph_builder.add_edge('summariser_node', 'expander_node')
+    inference_graph_builder.add_edge('expander_node', 'executor_node')
+    inference_graph_builder.add_edge('executor_node', 'evaluator_node')
+    inference_graph_builder.add_edge('evaluator_node', 'backpropagation_node')
+    inference_graph_builder.add_conditional_edges(
+        'backpropagation_node',
+        backpropagation_condition,
+        {
+            BackpropagationClassification.Select.value: 'selector_node',
+            BackpropagationClassification.Finalise.value: 'finaliser_node',
+        },
+    )
+    inference_graph_builder.add_edge('finaliser_node', END)
+    return inference_graph_builder.compile()
 
 
 async def create_main_graph(
@@ -59,7 +95,6 @@ async def create_main_graph(
     """创建主图"""
 
     mian_graph_builder = StateGraph(MainGraphState)
-
     mian_graph_builder.add_node('intent_classifier_node', intent_classifier_node)
     mian_graph_builder.add_node('intuition_chat_node', intuition_chat_node)
     mian_graph_builder.add_node(
