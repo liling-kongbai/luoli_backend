ee3df4054681abd8b82ac255096085722cab2d3d
liling-kongbai <kong4826@qq.com>
2026-03-10 12:46:30 +0800

    refactor: 更新 __init__.py

--- src/graph/type/__init__.py
+++ src/graph/type/__init__.py
@@ -1,6 +1,10 @@
 from .type import (
+    Backpropagation,
+    BackpropagationClassification,
     Intent,
     IntentClassification,
     Introspection,
     IntrospectionClassification,
+    Selection,
+    SelectionClassification,
 )
