c5191702ec0b3c0c6c30d906496c20806e6b60f2
liling-kongbai <kong4826@qq.com>
2026-03-10 14:18:59 +0800

    feat: 新增选择器节点

--- src/graph/node/inference_node.py
+++ src/graph/node/inference_node.py
@@ -1,6 +1,13 @@
+from logging import getLogger
 from math import log, sqrt
+from traceback import format_exc
 
-from ..type import LATSTreeNode
+from langchain_core.runnables.config import RunnableConfig
+
+from ..state import InferenceGraphState
+from ..type import LATSTreeNode, SelectionClassification
+
+logger = getLogger(__name__)
 
 
 def compute_uct_score(
@@ -53,5 +60,24 @@ def select_best_leaf_node(
             if uct_score > best_uct_score:
                 best_uct_score = uct_score
                 best_leaf_node = child_node
-
         current_node_id = best_leaf_node.id
+
+
+def selector_node(state: InferenceGraphState, config: RunnableConfig) -> dict:
+    """选择器节点"""
+
+    if state.iterate_count > config['configurable'].get('max_iterate_count'):
+        logger.warning('<selector_node> 迭代计数超过最大迭代计数，停止推理！！！')
+        return {'current_node_id': SelectionClassification.Finalise.value}
+
+    try:
+        return {
+            'current_node_id': select_best_leaf_node(
+                state.root_id,
+                state.tree_nodes,
+                config['configurable'].get('exploration_c'),
+            )
+        }
+    except Exception:
+        logger.error(f'<selector_node> 选择器节点报错！！！\n{format_exc()}')
+        return {'current_node_id': SelectionClassification.Finalise.value}
