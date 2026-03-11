b1ff4ee642a24139a737c1b42126914b04e2dd06
liling-kongbai <kong4826@qq.com>
2026-03-11 14:21:55 +0800

    refactor: 删除 common_node.py

--- src/graph/node/common_node.py
+++
@@ -1,8 +0,0 @@
-from langchain_core.runnables.config import RunnableConfig
-from langgraph.prebuilt.tool_node import ToolNode
-
-
-async def tools_node(state, config: RunnableConfig):
-    """工具节点"""
-
-    return await ToolNode(config['configurable'].get('tools')).ainvoke(state, config)
