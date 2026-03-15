822e2b171b91e3d4df16fecfcef2a68371306c0a
liling-kongbai <kong4826@qq.com>
2026-03-15 16:01:17 +0800

    feat: 新增加载

--- src/manager/config_manager.py
+++ src/manager/config_manager.py
@@ -1,3 +1,4 @@
+from psycopg_pool.pool_async import AsyncConnectionPool
 from pydantic.main import BaseModel
 
 
@@ -13,5 +14,20 @@ class DynamicConfig(BaseModel):
 class ConfigManager:
     """配置管理器"""
 
-    def __init__(self):
+    def __init__(self, user_id: str):
+        self._user_id: str | None = None
         self._dynamic_config = DynamicConfig()
+
+    async def load(self, connection_pool: AsyncConnectionPool):
+        """加载"""
+
+        async with connection_pool.connection() as connection:
+            async with connection.cursor() as cursor:
+                await cursor.execute(
+                    'SELECT * FROM dynamic_config_table WHERE user_id = %s',
+                    (self._user_id,),
+                )
+                result = await cursor.fetchone()
+                if result:
+                    self._dynamic_config = DynamicConfig(**result)
+        return self._dynamic_config
