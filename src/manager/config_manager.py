531d06c44efc6eeb1771f28a0d5edb7601f56504
liling-kongbai <kong4826@qq.com>
2026-03-15 14:28:22 +0800

    feat: 新增动态配置

---
+++ src/manager/config_manager.py
@@ -0,0 +1,10 @@
+from pydantic.main import BaseModel
+
+
+class DynamicConfig(BaseModel):
+    """动态配置"""
+
+    user_name: str = '理灵'
+    max_iterate_count: int = 10
+    summarise_depth: int = 3
+    exploration_c: float = 1.414
