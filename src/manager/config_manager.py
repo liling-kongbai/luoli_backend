1c0ea39ef87ebbe4755e9fa298d91415717ff9d1
liling-kongbai <kong4826@qq.com>
2026-03-15 16:30:09 +0800

    feat: 新增更新并微调其他内容

--- src/manager/config_manager.py
+++ src/manager/config_manager.py
@@ -15,19 +15,44 @@ class ConfigManager:
     """配置管理器"""
 
     def __init__(self, user_id: str):
-        self._user_id: str | None = None
+        self._user_id: str = user_id
         self._dynamic_config = DynamicConfig()
 
-    async def load(self, connection_pool: AsyncConnectionPool):
+    async def load(self, connection_pool: AsyncConnectionPool) -> DynamicConfig:
         """加载"""
 
         async with connection_pool.connection() as connection:
             async with connection.cursor() as cursor:
                 await cursor.execute(
-                    'SELECT * FROM dynamic_config_table WHERE user_id = %s',
+                    'SELECT user_name, max_iterate_count, summarise_depth, exploration_c FROM config_table WHERE user_id = %s',
                     (self._user_id,),
                 )
                 result = await cursor.fetchone()
                 if result:
-                    self._dynamic_config = DynamicConfig(**result)
+                    self._dynamic_config = DynamicConfig(
+                        user_name=result[0],
+                        max_iterate_count=result[1],
+                        summarise_depth=result[2],
+                        exploration_c=result[3],
+                    )
         return self._dynamic_config
+
+    async def update(
+        self, connection_pool: AsyncConnectionPool, dynamic_config: DynamicConfig
+    ):
+        """更新"""
+
+        self._dynamic_config = dynamic_config
+        async with connection_pool.connection() as connection:
+            async with connection.cursor() as cursor:
+                await cursor.execute(
+                    'UPDATE config_table SET user_name = %s, exploration_c = %s, max_iterate_count = %s, summarise_depth = %s WHERE user_id = %s',
+                    (
+                        dynamic_config.user_name,
+                        dynamic_config.exploration_c,
+                        dynamic_config.max_iterate_count,
+                        dynamic_config.summarise_depth,
+                        self._user_id,
+                    ),
+                )
+                await connection.commit()
