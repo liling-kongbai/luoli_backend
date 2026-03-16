1640d2b81129de4504b32e01999e4e066477ef1c
liling-kongbai <kong4826@qq.com>
2026-03-16 15:13:55 +0800

    fix: 微调 LLM 管理器

--- src/manager/llm_manager.py
+++ src/manager/llm_manager.py
@@ -2,6 +2,7 @@ from logging import getLogger
 from traceback import format_exc
 
 from langchain_core.language_models.chat_models import BaseChatModel
+from langchain_core.tools.base import BaseTool
 
 from ..utils import connect_deepseek_llm, connect_ollama_llm
 
@@ -12,82 +13,81 @@ class LLMManager:
     """LLM 管理器"""
 
     def __init__(self):
-        self._current_llm_platform_name: str | None = None
-        self._current_llm_model_name: str | None = None
-        self._current_llm_tools: list = []
-        self._current_llm: BaseChatModel | None = None
-        self._current_llm_bind_tools = self._current_llm
+        self._llm_platform_name: str | None = None
+        self._llm_model_name: str | None = None
+        self._tools: list[BaseTool] | None = None
 
-        self._current_embedding_model = None
+        self._llm: BaseChatModel | None = None
+        self._llm_bind_tools: BaseChatModel | None = None
+
+        self._embedding_model = None
 
     def clean_llm(self):
         """清理 llm"""
 
-        self._current_llm = None
-        self._current_llm_bind_tools = None
+        self._llm_bind_tools = None
+        self._llm = None
 
     def llm_bind_tools(self):
         """LLM 绑定工具"""
 
         try:
-            if self._current_llm_tools:
-                self._current_llm_bind_tools = self._current_llm.bind_tools(
-                    self._current_llm_tools
-                )
+            if self._tools:
+                self._llm_bind_tools = self._llm.bind_tools(self._tools)
             else:
-                self._current_llm_bind_tools = self._current_llm
+                self._llm_bind_tools = self._llm
         except Exception:
-            self._current_llm_bind_tools = self._current_llm
-            logger.error(
-                f'<llm_bind_tools> {self._current_llm_platform_name} 平台的 {self._current_llm_model_name} 绑定工具失败！！！\n{format_exc()}'
-            )
+            self._llm_bind_tools = self._llm
+            logger.error(f'<llm_bind_tools> LLM 绑定工具报错！！！\n{format_exc()}')
             raise
+
+    def set_tools(self, tools: list[BaseTool]):
+        """设置工具"""
+
+        self._tools = tools
+        if self._llm:
+            self.llm_bind_tools()
 
     def connect_llm(
         self, platform: str, model: str, temperature: float | None = None, **kwargs
     ):
         """连接 LLM"""
 
-        logger.info(f'<connect_llm> 连接 {platform} 平台的 {model}')
+        if not platform or not model:
+            self.clean_llm()
+            logger.info('<connect_llm> 平台或模型为空，LLM 已清理')
+            return
 
+        logger.info(f'<connect_llm> 连接 {platform} 平台的 {model}')
         try:
             match platform:
                 case 'ollama':
-                    self._current_llm = connect_ollama_llm(
+                    self._llm = connect_ollama_llm(
                         model, temperature=temperature, **kwargs
                     )
                 case 'deepseek':
-                    self._current_llm = connect_deepseek_llm(
+                    self._llm = connect_deepseek_llm(
                         model, temperature=temperature, **kwargs
                     )
                 case _:
                     raise ValueError(
-                        f'<connect_llm> 不支持 {platform} 平台的 {model}！！！'
+                        f'<connect_llm> 不支持 {platform} 平台，请检查代码逻辑！！！'
                     )
 
-            self._current_llm_platform_name = platform
-            self._current_llm_model_name = model
+            self._llm_platform_name = platform
+            self._llm_model_name = model
             self.llm_bind_tools()
+            logger.info(f'<connect_llm> 连接 {platform} 平台的 {model} 成功')
         except Exception:
-            logger.error(
-                f'<connect_llm> 连接 {platform} 平台的 {model} 失败！！！\n{format_exc()}'
-            )
+            logger.error(f'<connect_llm> 连接 LLM 报错！！！\n{format_exc()}')
             self.clean_llm()
             raise
-
-    def set_llm_tools(self, tools: list):
-        """设置 LLM 工具"""
-
-        self._current_llm_tools = tools
-        if self._current_llm is not None:
-            self.llm_bind_tools()
 
     def get_llm(self) -> BaseChatModel:
         """获取 LLM"""
 
-        if not self._current_llm_bind_tools:
-            error_message = '<get_llm> 当前未连接 LLM！！！'
-            logger.error(error_message)
-            raise ValueError(error_message)
+        if not self._llm_bind_tools:
+            logger.warning('<get_llm> 当前未连接 LLM，请检查代码逻辑！！！')
+            return
 
-        return self._current_llm_bind_tools
+        return self._llm_bind_tools
