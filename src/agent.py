044f78a59e919fed29bbdffeb16a57d57511e436
liling-kongbai <kong4826@qq.com>
2026-03-16 13:48:10 +0800

    feat: 新增处理请求

--- src/agent.py
+++ src/agent.py
@@ -1,7 +1,11 @@
+from asyncio.tasks import create_task
+from json import loads
 from logging import getLogger
 from traceback import format_exc
 
+from langchain_core.language_models.chat_models import BaseChatModel
 from langchain_core.runnables.config import RunnableConfig
+from langchain_core.tools.base import BaseTool
 
 from .manager import GraphManager, WebSocketConnectionManager
 
@@ -10,9 +14,19 @@ logger = getLogger(__name__)
 
 class Agent:
     def __init__(self):
+        self._user_id: str = 'liling'
+
         # 管理器相关
         self._websocket_connection_manager: WebSocketConnectionManager | None = None
         self._graph_manager: GraphManager | None = None
+
+        self._llm: BaseChatModel | None = None
+        self._tools: list[BaseTool] | None = None
+
+        self._user_name: str = '理灵'
+        self._max_iterate_count: int = 10
+        self._summarise_depth: int = 3
+        self._exploration_c: float = 1.414
 
     async def _process_chat(
         self,
@@ -50,3 +64,30 @@ class Agent:
         except Exception:
             logger.error(f'<_process_chat> 处理对话报错！！！\n{format_exc()}')
             raise
+
+    async def process_request(self, request: str):
+        """处理请求"""
+
+        data = loads(request)
+        action = data.get('action')
+        match action:
+            case 'chat':
+                create_task(
+                    self._process_chat(
+                        data.get('input'),
+                        RunnableConfig(
+                            configurable={
+                                'llm': self._llm,
+                                'tools': self._tools,
+                                'user_name': self._user_name,
+                                'max_iterate_count': self._max_iterate_count,
+                                'summarise_depth': self._summarise_depth,
+                                'exploration_c': self._exploration_c,
+                            },
+                        ),
+                        data.get('thread_id'),
+                        self._user_id,
+                    )
+                )
+            case _:
+                logger.warning(f'<process_request> 处理请求不支持的动作：{action}')
