201aaf3446b8c943a91e861c8bd162a3220e185d
liling-kongbai <kong4826@qq.com>
2026-03-17 14:33:51 +0800

    feat: 新增应用生命周期管理器和 FastAPI 相关内容

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
+from contextlib import asynccontextmanager
 from logging import getLogger
+from traceback import format_exc
+
+from fastapi import FastAPI
 
 from .set_async_logger import set_async_logger
+from .src.agent import Agent
 
 # 日志相关
 log_file_path = r'logs/luo_li_backend.log'
 log_listener = set_async_logger(log_file_path=log_file_path)
 logger = getLogger(__name__)
 logger.info(f'<main.py> 异步日志器已设置并启动，日志文件路径：{log_file_path}')
-log_listener.stop()
+
+
+agent: Agent | None = None
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """异步上下文管理器，应用生命周期管理器，让 FastAPI 自动运行应用核心服务的启动和关闭"""
+
+    try:
+        global agent
+        agent = Agent()
+
+        yield
+
+        agent = None
+    except Exception:
+        logger.error(f'<lifespan> 应用生命周期管理器报错！！！\n{format_exc()}')
+    finally:
+        log_listener.stop()
+
+
+app = FastAPI(lifespan=lifespan)
