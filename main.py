e784c608a4fae5eb6d412f22b3e1c5ca370c1f23
liling-kongbai <kong4826@qq.com>
2026-03-17 10:58:09 +0800

    fix: 微调 main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from logging import getLogger
 
-from set_async_logger import set_async_logger
+from .set_async_logger import set_async_logger
 
 # 日志相关
-log_file_path = 'logs/luo_li_backend.log'
+log_file_path = r'logs/luo_li_backend.log'
 log_listener = set_async_logger(log_file_path=log_file_path)
 logger = getLogger(__name__)
 logger.info(f'<main.py> 异步日志器已设置并启动，日志文件路径：{log_file_path}')
