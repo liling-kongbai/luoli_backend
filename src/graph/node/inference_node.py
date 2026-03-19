0e26be17abcdecec53ee36111ecd2e297b556cff
liling-kongbai <kong4826@qq.com>
2026-03-19 13:44:42 +0800

    fix: 微调推理层相关内容

--- src/graph/node/inference_node.py
+++ src/graph/node/inference_node.py
@@ -4,11 +4,8 @@ from logging import getLogger
 from math import log, sqrt
 from traceback import format_exc
 
-from langchain_core.messages.ai import AIMessage
-from langchain_core.messages.human import HumanMessage
 from langchain_core.runnables.config import RunnableConfig
 
-from ...manager import ToolManager
 from ..extractor import EvaluateGenerator, ExpandGenerator, FinalPlanGenerator
 from ..state import InferenceGraphState
 from ..type import ExecutionPlan, ExpandAction, LATSTreeNode, SelectionClassification
@@ -90,16 +87,18 @@ def selector_node(state: InferenceGraphState, config: RunnableConfig) -> dict:
 
 
 def get_context_nodes_trajectory(
-    tree_nodes: dict[str, LATSTreeNode], leaf_node_id: str
+    tree_nodes: dict[str, LATSTreeNode], leaf_node_id: str, context_depth: int
 ) -> list[LATSTreeNode]:
     """获取上下文节点轨迹"""
 
+    current_context_depth = 0
     context_nodes_trajectory = []
     current_node_id = leaf_node_id
-    while current_node_id:
+    while current_node_id and current_context_depth <= context_depth:
         current_node = tree_nodes[current_node_id]
         context_nodes_trajectory.append(current_node)
         current_node_id = current_node.parent_id
+        current_context_depth += 1
     return context_nodes_trajectory[::-1]
 
 
@@ -109,54 +108,36 @@ async def expander_node(state: InferenceGraphState, config: RunnableConfig) -> d
     current_node_id = state.current_node_id
     tree_nodes = state.tree_nodes
     current_node = tree_nodes[current_node_id]
-    current_node_depth = current_node.depth
-    recent_depth = current_node_depth % config['configurable'].get('summarise_depth')
-    if recent_depth == 0:
-        recent_depth = 1
 
-    current_node_context = []
+    current_node_context = ''
+
+    recent_depth = current_node.depth % config['configurable'].get('summarise_depth')
     if current_node_summary := current_node.summary:
-        current_node_context.append(
-            HumanMessage(f'前情提要（记忆总结）：{current_node_summary}')
-        )
+        current_node_context += f'前情提要/记忆总结：{current_node_summary}\n'
 
-    context_nodes_trajectory = get_context_nodes_trajectory(tree_nodes, current_node_id)
-    recent_context_nodes_trajectory = context_nodes_trajectory[-recent_depth:]
-    for node in recent_context_nodes_trajectory:
-        if node_action := node.action:
-            current_node_context.append(
-                AIMessage(
-                    f'内部思考：{node_action.thought}\n需要调用的工具：{node_action.tool_name}\n需要调用的工具的参数：{node_action.tool_args}'
-                )
-            )
-        if node_observation := node.observation:
-            current_node_context.append(
-                HumanMessage(f'工具运行情况：{node_observation}')
-            )
+    if recent_depth != 0 or not current_node_summary:
+        context_nodes_trajectory = get_context_nodes_trajectory(
+            tree_nodes, current_node_id, recent_depth
+        )
+        for node in context_nodes_trajectory:
+            if node_action := node.action:
+                current_node_context += f'内部思考：{node_action.thought}\n需要调用的工具：{node_action.tool_name}\n需要调用的工具的参数：{node_action.tool_args}\n'
+            if node_observation := node.observation:
+                current_node_context += f'工具运行情况：{node_observation}\n'
 
     try:
         chain = ExpandGenerator(config['configurable'].get('llm')).get_extractor_chain()
         result = await chain.ainvoke(
             {
                 'user_input_content': state.user_input_content,
-                'summary': current_node_context,
+                'current_node_context': current_node_context,
                 'input': '开始生成',
             },
             config,
         )
-
-        if not (candidates := result.candidates):
-            updated_node = current_node.model_copy()
-            updated_node.is_completed = True
-            return {
-                'tree_nodes': {current_node_id: updated_node},
-                'llm_call_count': state.llm_call_count + 1,
-                'candidates': [],
-            }
-
         return {
-            'candidates': candidates,
             'llm_call_count': state.llm_call_count + 1,
+            'candidates': result.candidates or [],
         }
     except Exception:
         logger.error(f'<expander_node> 扩展器节点报错！！！\n{format_exc()}')
@@ -166,48 +147,48 @@ async def expander_node(state: InferenceGraphState, config: RunnableConfig) -> d
 async def process_expand_action(
     config: RunnableConfig,
     expand_action: ExpandAction,
-    tool_manager: ToolManager,
     current_node_id: str,
     current_node_depth: int,
     current_node_summary: str | None,
 ) -> LATSTreeNode:
     """处理扩展行动"""
 
-    tool = tool_manager.get_tool(tool_name := expand_action.tool_name)
-
-    if not tool:
-        logger.warning(f'<process_expand_action> 工具 {tool_name} 不存在！！！')
-        observation = f'工具 {tool_name} 不存在！！！'
-    elif not getattr(tool, 'is_safe', False):
-        logger.warning(
-            f'<process_expand_action> 工具 {tool_name} 不安全，已模拟运行，运行成功！！！'
-        )
-        observation = f'工具 {tool_name} 不安全，已模拟运行，运行成功！！！'
-    else:
-        try:
-            tool_args = expand_action.tool_args or {}
-            result = await tool.ainvoke(tool_args, config)
-            observation = (
-                result if isinstance(result, str) else dumps(result, ensure_ascii=False)
-            )
-        except Exception:
-            logger.error(
-                f'<process_expand_action> 处理扩展行动报错！！！\n{format_exc()}'
-            )
-            observation = f'工具 {tool_name} 执行失败。\n工具参数：{tool_args}\n'
+    if tool_name := expand_action.tool_name:
+        if tool := config['configurable'].get('tool_manager').get_tool(tool_name):
+            if getattr(tool, 'is_safe', False):
+                logger.warning(
+                    f'<process_expand_action> 工具 {tool_name} 不安全，已模拟运行，运行完成！！！'
+                )
+                observation = f'工具 {tool_name} 不安全，已模拟运行，运行完成！！！'
+            else:
+                try:
+                    tool_args = expand_action.tool_args or {}
+                    result = await tool.tool.ainvoke(tool_args, config)
+                    observation = (
+                        result
+                        if isinstance(result, str)
+                        else dumps(result, ensure_ascii=False)
+                    )
+                except Exception:
+                    logger.error(
+                        f'<process_expand_action> 处理扩展行动报错！！！\n{format_exc()}'
+                    )
+                    observation = f'工具 {tool_name} 执行失败。\n工具参数：{tool_args}\n{format_exc()}'
+        else:
+            logger.warning(f'<process_expand_action> 工具 {tool_name} 不存在！！！')
+            observation = f'工具 {tool_name} 不存在！！！'
     return LATSTreeNode(
         parent_id=current_node_id,
-        action=expand_action,
-        observation=observation,
         depth=current_node_depth + 1,
         summary=current_node_summary,
+        action=expand_action,
+        observation=observation,
     )
 
 
 async def executor_node(state: InferenceGraphState, config: RunnableConfig) -> dict:
     """执行器节点"""
 
-    tool_manager = config['configurable'].get('tool_manager')
     current_node_id = state.current_node_id
     current_node = state.tree_nodes[current_node_id]
 
@@ -216,7 +197,6 @@ async def executor_node(state: InferenceGraphState, config: RunnableConfig) -> d
             process_expand_action(
                 config,
                 expand_action,
-                tool_manager,
                 current_node_id,
                 current_node.depth,
                 current_node.summary,
@@ -228,9 +208,11 @@ async def executor_node(state: InferenceGraphState, config: RunnableConfig) -> d
 
     new_nodes = {}
     for result in results:
-        new_nodes[result.id] = result
+        if not isinstance(result, Exception):
+            new_nodes[result.id] = result
     parent_node = current_node.model_copy()
     parent_node.children_ids.extend(list(new_nodes.keys()))
+    new_nodes[current_node_id] = parent_node
     return {'tree_nodes': new_nodes}
 
 
